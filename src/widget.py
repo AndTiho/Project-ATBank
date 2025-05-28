d90ad3cd0d29e329048e7aef142acec502485fb6
Andrei Tihomirov <moon200505@mail.ru>
2025-05-28 07:24:26 +0300

    widget added

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(account_data: str) -> Any:
+    full_data_list: list = account_data.split(" ")
+    list_alpha: list = [i for i in full_data_list if i.isalpha()]
+    list_digit: list = [i for i in full_data_list if i.isdigit()]
+
+    if len(list_digit[0]) == 16:
+        return " ".join(list_alpha) + " " + get_mask_card_number("".join(list_digit))
+    elif len(list_digit[0]) == 20:
+        return " ".join(list_alpha) + " " + get_mask_account("".join(list_digit))
+    else:
+        return None
+
+
