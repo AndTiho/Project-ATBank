a5d3c8062949976a6c820f9f53d32b80156c6d4b
Andrei Tihomirov <moon200505@mail.ru>
2025-05-31 09:17:09 +0300

    homework chenges 2.0

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,18 @@
 def filter_by_state(users_data_list: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """Функция создаёт новый список из заданного списка фильтруя по заданному значению"""
-    filtered_users_data_list = list()
-    for i in users_data_list:
-        for key, value in i.items():
+    """
+    Функция создаёт новый список из заданного списка фильтруя по заданному значению
+    """
+    filtered_users_list: list = list()
+    for user in users_data_list:
+        for key, value in user.items():
             if value == state:
-                filtered_users_data_list.append(i)
-    return filtered_users_data_list
+                filtered_users_list.append(user)
+    return filtered_users_list
 
 
 def sort_by_date(user_data_list: list[dict], sort_by: bool = True) -> list[dict]:
-    """Функция для сортировки данных пользователя по дате"""
-    sorted_user_data_list = sorted(user_data_list, key=lambda user_data: user_data["date"], reverse=sort_by)
-    return sorted_user_data_list
+    """
+    Функция для сортировки данных пользователя по дате
+    """
+    sorted_user_list: list = sorted(user_data_list, key=lambda user_data: user_data["date"], reverse=sort_by)
+    return sorted_user_list
