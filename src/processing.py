1ddc0483e28ca8fc5d2521a36e0d6363e683495f
Andrei Tihomirov <moon200505@mail.ru>
2025-06-02 17:04:31 +0300

    add tests for processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,26 @@
-def filter_by_state(users_data_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+from typing import Union
+
+
+def filter_by_state(users_data_list: list[dict], state: str = "EXECUTED") -> Union[list[dict], str]:
     """
     Функция создаёт новый список из заданного списка фильтруя по заданному значению
     """
     filtered_users_list: list = list()
+    if isinstance(users_data_list, list):
+        for user in users_data_list:
+            for key, value in user.items():
+                if value == state:
+                    filtered_users_list.append(user)
+        return filtered_users_list
+    return "Не корректно введены данные"
 
-    for user in users_data_list:
-        for key, value in user.items():
-            if value == state:
-                filtered_users_list.append(user)
 
-    return filtered_users_list
-
-
-def sort_by_date(users_data_list: list[dict], sort_by: bool = True) -> list[dict]:
+def sort_by_date(users_data_list: list[dict], sort_by: bool = True) -> Union[list[dict], str]:
     """
     Функция для сортировки данных пользователя по дате
     """
-    sorted_users_list: list = sorted(users_data_list, key=lambda user_data: user_data["date"], reverse=sort_by)
-    return sorted_users_list
+    if isinstance(users_data_list, list):
+        if all("date" in key for key in users_data_list):
+            sorted_users_list: list = sorted(users_data_list, key=lambda user_data: user_data["date"], reverse=sort_by)
+            return sorted_users_list
+    return "Не корректно введены данные"
