e0923cd0d3825c87c8f8b2a60fcbd44103800058
Andrei Tihomirov <moon200505@mail.ru>
2025-07-01 16:59:26 +0300

    add logger utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,39 @@
 import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='../logs/utils.log',
+                    filemode='w',
+                    encoding='utf-8')
+
+json_to_python_logger = logging.getLogger('app.json_to_python')
 
 
 def json_to_python_data(file_path: str) -> list:
     """Функция принимает ПУТЬ к файлу JSON и преобразует его в Python список,
     либо возвращает пустой Python список.
      Пример пути к файлу: '../data/operations.json' """
+    json_to_python_logger.info(f'Начало работы программы')
+
     python_data = []
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             try:
                 python_data = json.load(file)
             except json.JSONDecodeError:
+                json_to_python_logger.warning('Ошибка декодирования файла. Программа возвращает пустой список.')
                 print("Ошибка декодирования файла, возвращаем пустой список")
                 return python_data
     except TypeError:
+        json_to_python_logger.warning('Не верно указан путь. Программа возвращает пустой список.')
         print("Не верно указан путь")
         return python_data
     except FileNotFoundError:
+        json_to_python_logger.warning('Файл не найден. Программа возвращает пустой список.')
         print("Файл не найден, возвращаем пустой список")
         return python_data
+    json_to_python_logger.info(f'Программа отработала корректно. Завершение работы')
     return python_data
 
+print(json_to_python_data(''))
