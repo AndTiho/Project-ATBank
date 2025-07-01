3b08bf018276252d0fcc511829f740757592bb02
Andrei Tihomirov <moon200505@mail.ru>
2025-07-01 17:07:33 +0300

    update with black/isort/flake8/mypy - zero errors

--- src/masks.py
+++ src/masks.py
@@ -1,32 +1,35 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    filename='../logs/masks.log',
-                    filemode='w',
-                    encoding='utf-8')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="../logs/masks.log",
+    filemode="w",
+    encoding="utf-8",
+)
 
-mask_account_logger = logging.getLogger('app.mask_acc')
-mask_card_logger = logging.getLogger('app.mask_card')
+mask_account_logger = logging.getLogger("app.mask_acc")
+mask_card_logger = logging.getLogger("app.mask_card")
+
 
 def get_mask_card_number(card_number: str) -> str:
     """
     Функция принимает на вход номер карты и возвращает ее маску.
     """
-    mask_card_logger.info(f'Запуск модуля')
-    mask_card_logger.info(f'Получение данных {card_number}')
+    mask_card_logger.info("Запуск модуля")
+    mask_card_logger.info(f"Получение данных {card_number}")
 
     if not isinstance(card_number, str):
-        mask_card_logger.error(f'{card_number}: не является строкой. Завершение работы')
+        mask_card_logger.error(f"{card_number}: не является строкой. Завершение работы")
         return "Не корректно введены данные"
     mask_card_number: list = list("".join(num for num in card_number if num.isdigit()))
     if len(mask_card_number) != 16:
-        mask_card_logger.error(f'{card_number}: содержит неверное число символов (16). Завершение работы')
+        mask_card_logger.error(f"{card_number}: содержит неверное число символов (16). Завершение работы")
         return "Не корректно введены данные"
     for i in range(5, 11):
         mask_card_number[i] = "*"
 
-    mask_card_logger.info(f'Функция отработала корректно. Завершение работы')
+    mask_card_logger.info("Функция отработала корректно. Завершение работы")
     return " ".join("".join(mask_card_number[i : i + 4]) for i in range(0, len(mask_card_number), 4))
 
 
@@ -34,14 +37,16 @@ def get_mask_account(account: str) -> str:
     """
     Функция принимает на вход номер счета и возвращает его маску.
     """
-    mask_account_logger.info(f'Запуск работы модуля.')
+    mask_account_logger.info("Запуск работы модуля.")
 
     if not isinstance(account, str):
-        mask_account_logger.error(f'{account}: введённые данные не являются строкой. Завершение работы..')
+        mask_account_logger.error(f"{account}: введённые данные не являются строкой. Завершение работы..")
         return "Не корректно введены данные"
     mask_account: str = "".join(num for num in account if num.isdigit())
     if len(mask_account) != 20:
-        mask_account_logger.error(f'{len(mask_account)}: Длина номера не соответствует требованиям в 20 символов. Завершение работы..')
+        mask_account_logger.error(
+            f"{len(mask_account)}: Длина номера не соответствует требованиям в 20 символов. Завершение работы.."
+        )
         return "Не корректно введены данные"
-    mask_account_logger.info(f'Программа отработала корректно. Завершение работы')
+    mask_account_logger.info("Программа отработала корректно. Завершение работы")
     return "".join(["**", mask_account[-4:]])
