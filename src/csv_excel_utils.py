3dc0b29b50795c09315782423dc0d6d1bb6a78fe
Andrei Tihomirov <moon200505@mail.ru>
2025-07-05 13:59:30 +0300

    pass flake8/mypy

--- src/csv_excel_utils.py
+++ src/csv_excel_utils.py
@@ -1,18 +1,20 @@
-from typing import Any
 import csv
+from typing import Any
+
 import pandas as pd
+
 
 def csv_to_python_data(file_path: str) -> Any:
     """Функция принимает ПУТЬ к файлу CSV и преобразует его в Python список словарей,
     либо возвращает пустой Python список.
-     Пример пути к файлу: './data/transactions.csv' """
+     Пример пути к файлу: './data/transactions.csv'"""
 
     if not isinstance(file_path, str):
         print("'Путь к файлу' должен быть строкой. Возвращаем пустой список.")
         return []
     try:
-        with open(file_path, "r",  encoding="utf-8") as file:
-            python_data = csv.DictReader(file, delimiter= ';')
+        with open(file_path, "r", encoding="utf-8") as file:
+            python_data = csv.DictReader(file, delimiter=";")
             csv_list = list(python_data)
     except OSError:
         print("Ошибка декодирования файла, возвращаем пустой список.")
@@ -26,14 +28,14 @@ def csv_to_python_data(file_path: str) -> Any:
 def excel_to_python_data(file_path: str) -> Any:
     """Функция принимает ПУТЬ к файлу CSV и преобразует его в Python список словарей,
     либо возвращает пустой Python список.
-     Пример пути к файлу: './data/transactions_excel.xlsx' """
+     Пример пути к файлу: './data/transactions_excel.xlsx'"""
 
     if not isinstance(file_path, str):
         print("'Путь к файлу' должен быть строкой. Возвращаем пустой список.")
         return []
     try:
         df = pd.read_excel(file_path)
-        excel_data = df.to_dict('records')
+        excel_data = df.to_dict("records")
     except ValueError:
         print("Не верно указан путь. Возвращаем пустой список.")
         return []
