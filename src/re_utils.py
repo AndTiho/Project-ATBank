7ef29361ec4c63a6d7a6eab7957adfd36fc0ad1f
Andrei Tihomirov <moon200505@mail.ru>
2025-07-09 18:30:29 +0300

    add test_re_utils_py / update re_utils.py =)

--- src/re_utils.py
+++ src/re_utils.py
@@ -1,8 +1,10 @@
 import re
-from collections import Counter, defaultdict
-
+from collections import Counter
+# from csv_excel_utils import csv_to_python_data
 
 def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    if not isinstance(data, list):
+        raise TypeError("Входные данные не являются списком")
     new_dict = []
     pattern = re.compile(search)
     for dict_data in data:
@@ -12,10 +14,16 @@ def process_bank_search(data: list[dict], search: str) -> list[dict]:
 
 
 def process_bank_operations(data: list[dict], categories: list) -> dict:
-    new_dict: dict = defaultdict()
-    descriptions = [x["description"] for x in data]
+    if not isinstance(data, list):
+        raise TypeError("Входные данные не являются списком")
+    new_dict: dict = {}
+    descriptions = [x.get("description") for x in data]
     counter = Counter(descriptions)
     for i in categories:
         result = counter[i]
         new_dict[i] = result
     return new_dict
+
+# need_data = csv_to_python_data('./data/transactions.csv')
+# need_data = []
+# print(process_bank_operations(need_data, ["Открытие вклада"]))
---
+++ tests/test_re_utils.py
@@ -0,0 +1,45 @@
+import pytest
+from src.re_utils import process_bank_search, process_bank_operations
+
+
+def test_process_bank_search(transactions_data):
+    assert process_bank_search(transactions_data, 'Перевод организации') == [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+def test_process_bank_search_empty_list(empty_list):
+    assert process_bank_search(empty_list, "Перевод организации") == []
+
+def test_process_bank_search_invalid_data_type(not_list):
+    with pytest.raises(TypeError) as exc_info:
+        process_bank_search(not_list, "Перевод организации")
+    assert "Входные данные не являются списком" in str(exc_info.value)
+
+def test_process_bank_operations(transactions_data):
+    assert (process_bank_operations(transactions_data, ['Перевод организации','Перевод с карты на карту'])
+            == {'Перевод организации': 2, 'Перевод с карты на карту': 1})
+
+def test_process_bank_operations_empty_list(empty_list):
+    assert process_bank_operations(empty_list, ['Перевод организации','Перевод с карты на карту']) == {'Перевод организации': 0, 'Перевод с карты на карту': 0}
+
+def test_process_bank_operations_invalid_data_type(not_list):
+    with pytest.raises(TypeError) as exc_info:
+        process_bank_operations(not_list, ["Перевод организации"])
+    assert "Входные данные не являются списком" in str(exc_info.value)
